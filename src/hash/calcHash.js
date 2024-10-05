c00c161e1f6b9ab6ff457c794fd9f3608216a9ba
Raman Ivashkevich <rivashkevich@solvd.com>
2024-10-05 16:23:48 +0200

    feat: implement hash tasks

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,28 @@
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { createHash } from 'crypto';
+
 const calculateHash = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filesDir = path.join(__dirname, 'files');
+
+    const hash = createHash('sha256');
+    const readStream = fs.createReadStream(`${filesDir}/fileToCalculateHashFor.txt`);
+
+    readStream.on('data', (chunk) => {
+      hash.update(chunk);
+    });
+
+    readStream.on('end', () => {
+      const hashHex = hash.digest('hex');
+      console.log(hashHex);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await calculateHash();
