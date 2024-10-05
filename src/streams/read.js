ba3f20efd3cfd11fd25768cd42850e8c4e248a59
Raman Ivashkevich <rivashkevich@solvd.com>
2024-10-05 16:26:54 +0200

    feat: implement streams tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,29 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filesDir = path.join(__dirname, 'files');
+
+    const readStream = fs.createReadStream(`${filesDir}/fileToRead.txt`, { encoding: "utf8" });
+
+    readStream.on('data', (chunk) => {
+      process.stdout.write(chunk);
+    });
+
+    readStream.on('end', () => {
+      console.log('\nEnd of stream');
+    });
+
+    readStream.on('error', () => {
+      throw new Error('Stream operation failed');
+    });
+  } catch (error) {
+    console.error(error)
+  }
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const write = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filesDir = path.join(__dirname, 'files');
+
+    const writableStream = fs.createWriteStream(`${filesDir}/fileToWrite.txt`);
+
+    console.log('Ctrl + D to stop writing');
+    process.stdin.pipe(writableStream);
+
+    writableStream.on('finish', () => {
+      console.log('Data written to file successfully.');
+    });
+
+    writableStream.on('error', () => {
+      throw new Error('Stream operation failed');
+    });
+  } catch (error) {
+    console.error(error)
+  }
 };
 
 await write();
