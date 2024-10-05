8a28e1ac29d37768a0bb702ef9531f141dd93c23
Raman Ivashkevich <rivashkevich@solvd.com>
2024-10-05 16:27:39 +0200

    feat: implement zip tasks

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,27 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const filesDir = path.join(__dirname, 'files');
+
+    const pipelinePromise = promisify(pipeline);
+
+    const gzip = createGzip();
+    const source = fs.createReadStream(`${filesDir}/fileToCompress.txt`);
+    const destination = fs.createWriteStream(`${filesDir}/archive.gz`);
+
+    await pipelinePromise(source, gzip, destination);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 await compress();
