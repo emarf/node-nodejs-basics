cad67da73aa1703c0f4ea5f828e3d6afa8f781e8
Raman Ivashkevich <rivashkevich@solvd.com>
2024-10-05 16:29:33 +0200

    feat: implement cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,17 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const filesDir = path.join(__dirname, 'files');
+
+  const cp = spawn('node', [`${filesDir}/script.js`, ...args]);
+
+  process.stdin.pipe(cp.stdin);
+  cp.stdout.pipe(process.stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['someArgument1', 'someArgument2']);
